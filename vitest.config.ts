3601f090ad440f642fba99a70bb8b9c56945ba24
Andrew Ferreira <andrew.shien2@gmail.com>
2023-11-21 03:20:36 -0300

    style: format files

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,18 +2,18 @@ import { defineConfig } from 'vitest/config';
 import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
-    plugins: [vue()],
-    test: {
-        globals: false,
-        browser: {
-            name: 'chrome',
-            enabled: true,
-            headless: true
-        },
-        watch: false,
-        coverage: {
-            provider: 'istanbul',
-            reporter: ['html']
-        }
+  plugins: [vue()],
+  test: {
+    globals: false,
+    browser: {
+      name: 'chrome',
+      enabled: true,
+      headless: true
+    },
+    watch: false,
+    coverage: {
+      provider: 'istanbul',
+      reporter: ['html']
     }
+  }
 });
