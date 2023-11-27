56b7cd3b7a31e39a938dce92f52cb9c88ffc134a
ferreira-tb <andrew.shien2@gmail.com>
2023-11-27 15:55:56 -0300

    build: use miho for build

--- miho.config.ts
+++ miho.config.ts
@@ -1,4 +1,9 @@
+import process from 'node:process';
+import path from 'node:path';
+import fs from 'node:fs/promises';
 import { Octokit } from '@octokit/core';
+import { existsSync as exists } from 'node:fs';
+import { execa, type Options as ExecaOptions } from 'execa';
 import { defineConfig, PackageManager } from 'miho';
 
 export default defineConfig({
@@ -9,7 +14,25 @@ export default defineConfig({
     all: true
   },
   jobs: {
-    build: true,
+    build: async () => {
+      const dist = path.join(process.cwd(), 'packages/manatsu/dist');
+      const components = path.join(process.cwd(), 'packages/components/dist');
+
+      if (exists(dist)) {
+        await fs.rm(dist, { recursive: true });
+      }
+
+      const options: ExecaOptions = { stdio: 'inherit' };
+      await Promise.all([
+        execa('pnpm', ['-F', 'manatsu', 'build'], options),
+        execa('pnpm', ['-F', 'components', 'build'], options)
+      ]);
+
+      const dts = path.join(components, 'index.d.ts');
+      await fs.rename(dts, path.join(dist, 'index.d.ts'));
+
+      await execa('pnpm', ['minify'], options);
+    },
     publish: async () => {
       const { version } = await import('./package.json');
       const { GITHUB_TOKEN } = await import('./config.json');
