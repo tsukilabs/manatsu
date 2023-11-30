faef678e6c9cbc8037604baae9e085d7e4b37fc3
Andrew Ferreira <andrew.shien2@gmail.com>
2023-11-29 21:00:50 -0300

    feat: use computed for colors

--- packages/components/button/Button.vue
+++ packages/components/button/Button.vue
@@ -1,31 +1,45 @@
 <!-- eslint-disable @typescript-eslint/no-unsafe-return -->
 <script setup lang="ts">
 import { computed } from 'vue';
-import FilledButton from './FilledButton.vue';
 
 interface Props {
-  readonly type?: 'filled';
+  readonly type?: 'filled' | 'tonal';
 }
 
 const props = withDefaults(defineProps<Props>(), {
   type: 'filled'
 });
 
-const button = computed(() => {
+const color = computed(() => {
   switch (props.type) {
     case 'filled':
-      return FilledButton;
+      return 'var(--mana-color-on-primary)';
+    case 'tonal':
+      return 'var(--mana-color-on-secondary-container)';
     default:
-      return FilledButton;
+      return 'var(--mana-color-on-primary)';
+  }
+});
+
+const bgColor = computed(() => {
+  switch (props.type) {
+    case 'filled':
+      return 'var(--mana-color-primary)';
+    case 'tonal':
+      return 'var(--mana-color-secondary-container)';
+    default:
+      return 'var(--mana-color-primary)';
   }
 });
 </script>
 
 <template>
   <div class="m-button" role="none">
-    <component :is="button">
-      <slot></slot>
-    </component>
+    <button type="button">
+      <span class="m-button-content">
+        <slot></slot>
+      </span>
+    </button>
   </div>
 </template>
 
@@ -41,11 +55,26 @@ const button = computed(() => {
   button {
     cursor: pointer;
     border: none;
-    background-color: transparent;
+    border-radius: var(--mana-button-border-radius);
+    background-color: v-bind('bgColor');
     padding: var(--mana-button-padding);
-    color: inherit;
+    min-width: var(--mana-button-min-width);
+    min-height: var(--mana-button-height);
+    color: v-bind('color');
+    line-height: var(--mana-button-line-height);
     font-family: inherit;
     text-align: center;
+
+    transition: {
+      property: filter;
+      duration: 200ms;
+      timing-function: ease-in-out;
+    }
+
+    &:hover,
+    &:active {
+      filter: contrast(150%);
+    }
   }
 }
 </style>
--- packages/playground/src/App.vue
+++ packages/playground/src/App.vue
@@ -6,10 +6,11 @@ import { MButton } from '@manatsu/components';
   <main class="flex flex-col gap-4">
     <div class="flex flex-col gap-2">
       <div>Hello world!</div>
-      <MButton>Inside flex-col</MButton>
-      <MButton>Inside flex-col with more text</MButton>
+      <MButton type="filled">Filled</MButton>
+      <MButton type="tonal">Tonal</MButton>
     </div>
     <div class="flex gap-2">
+      <MButton>A</MButton>
       <MButton>Inside flex</MButton>
       <MButton>Inside flex with more text</MButton>
     </div>
