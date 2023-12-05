a4ba905eed8de6db789975519858d5b1aa52f15d
Andrew Ferreira <andrew.shien2@gmail.com>
2023-12-04 22:06:47 -0300

    build: update miho config

--- miho.config.ts
+++ miho.config.ts
@@ -1,6 +1,6 @@
-import path from 'node:path';
 import fs from 'node:fs/promises';
-import process from 'node:process';
+import { cwd } from 'node:process';
+import { basename, join } from 'node:path';
 import { existsSync as exists } from 'node:fs';
 import { Octokit } from '@octokit/core';
 import { PackageManager, defineConfig } from 'miho';
@@ -8,39 +8,45 @@ import { type Options as ExecaOptions, execa } from 'execa';
 
 type PackageName = 'manatsu' | 'components' | 'themes';
 
-function getDistDir(packageName: PackageName) {
-  return path.join(process.cwd(), `packages/${packageName}/dist`);
-}
-
-function buildPackage(packageName: PackageName) {
-  const options: ExecaOptions = { stdio: 'inherit' };
-  return execa('pnpm', ['-F', packageName, 'build'], options);
-}
-
 export default defineConfig({
   packageManager: PackageManager.PNPM,
   recursive: true,
   commit: {
     push: true,
-    all: true
+    all: true,
+    message: (miho) => {
+      const pkg = miho.getPackageByName('manatsu');
+      if (!pkg) throw new Error('No package found.');
+      const { newVersion } = pkg;
+      if (!newVersion) return null;
+      return `chore: bump version to ${newVersion}`;
+    }
   },
   jobs: {
     skip: ['publish'],
     build: async () => {
-      const manatsu = getDistDir('manatsu');
-      const components = getDistDir('components');
+      const root = join(cwd(), 'dist');
+      const manatsu = dist('manatsu');
+      const components = dist('components');
 
-      if (exists(manatsu)) {
-        await fs.rm(manatsu, { recursive: true });
+      if (exists(root)) {
+        await fs.rm(root, { recursive: true });
       }
 
       // Build.
+      await fs.mkdir(root, { recursive: true });
       const packages = ['manatsu', 'components', 'themes'] as const;
-      await Promise.all(packages.map((pkg) => buildPackage(pkg)));
+      await Promise.all(packages.map(build));
 
       // Copy files.
-      const dts = path.join(components, 'index.d.ts');
-      await fs.copyFile(dts, path.join(manatsu, 'index.d.ts'));
+      let files = await fs.readdir(manatsu);
+      files = files.map((f) => join(manatsu, f));
+      await Promise.all(
+        files.map((f) => fs.copyFile(f, join(root, basename(f))))
+      );
+
+      const dts = join(components, 'index.d.ts');
+      await fs.copyFile(dts, join(root, 'index.d.ts'));
     },
     publish: async () => {
       const { version } = await import('./package.json');
@@ -48,8 +54,8 @@ export default defineConfig({
       const octokit = new Octokit({ auth: GITHUB_TOKEN });
 
       await octokit.request('POST /repos/{owner}/{repo}/releases', {
-        tag_name: version,
-        name: version,
+        tag_name: `v${version}`,
+        name: `v${version}`,
         draft: false,
         prerelease: true,
         generate_release_notes: true,
@@ -63,3 +69,12 @@ export default defineConfig({
     }
   }
 });
+
+function dist(packageName: PackageName) {
+  return join(cwd(), `packages/${packageName}/dist`);
+}
+
+function build(packageName: PackageName) {
+  const options: ExecaOptions = { stdio: 'inherit' };
+  return execa('pnpm', ['-F', packageName, 'build'], options);
+}
