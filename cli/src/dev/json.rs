41c4a43256ae363fe81bf8a73b81eafe9467f7fb
Andrew Ferreira <andrew.shien2@gmail.com>
2023-12-27 19:30:07 -0300

    chore: update config

--- cli/src/dev/json.rs
+++ cli/src/dev/json.rs
@@ -14,19 +14,20 @@ pub struct Package {
   pub version: String,
 }
 
-/// Read the local `config.json` file.
 fn read(file: &str) -> Result<String> {
   let json = env::current_dir()?.join(file);
   let content = fs::read_to_string(json)?;
   Ok(content)
 }
 
+/// Read the `config.json` file.
 pub fn read_config() -> Result<Config> {
   let content = read("config.json")?;
   let config: Config = serde_json::from_str(&content)?;
   Ok(config)
 }
 
+/// Read the `package.json` file.
 pub fn read_package() -> Result<Package> {
   let content = read("package.json")?;
   let package: Package = serde_json::from_str(&content)?;
