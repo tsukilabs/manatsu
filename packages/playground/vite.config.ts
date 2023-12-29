6d371c5b22ba01ab03587f78a02698c877ebc2c0
Andrew Ferreira <andrew.shien2@gmail.com>
2023-12-29 15:13:46 -0300

    chore: bump version

--- packages/playground/vite.config.ts
+++ packages/playground/vite.config.ts
@@ -3,7 +3,10 @@ import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
   plugins: [vue()],
+  root: __dirname,
   build: {
+    outDir: 'dist',
+    emptyOutDir: true,
     minify: false
   }
 });
