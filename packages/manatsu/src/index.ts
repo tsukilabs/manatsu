28c59c2dbbc5055e52c08998758fd2c6269dfbc3
Andrew Ferreira <andrew.shien2@gmail.com>
2024-01-01 19:34:05 -0300

    core: refactor plugin

--- packages/manatsu/src/index.ts
+++ packages/manatsu/src/index.ts
@@ -1,45 +1,6 @@
 import './style/main.scss';
-import { type Plugin, effectScope, ref, watchEffect } from 'vue';
-import { tryOnScopeDispose, usePreferredDark } from '@vueuse/core';
-import * as symbol from './symbol';
-import { setDarkMode } from './plugin';
 
+export * from './plugin';
 export * from './symbol';
 export * from '@manatsu/components/src/index.ts';
 export * from '@manatsu/composables/src/index.ts';
-
-export interface ManatsuOptions {
-  /** @default 'auto' */
-  darkMode?: boolean | 'auto';
-}
-
-export function createManatsu(options: ManatsuOptions = {}): Plugin {
-  const manatsu: Plugin = {
-    install(app) {
-      const darkMode = ref(options.darkMode ?? 'auto');
-      const preferredDark = usePreferredDark();
-      app.provide(symbol.darkModeKey, darkMode);
-
-      /** @see https://github.com/vuejs/core/pull/8801 */
-      /** @see https://github.com/vuejs/core/issues/8787 */
-      const scope = effectScope();
-      scope.run(() => {
-        watchEffect(() => {
-          if (darkMode.value === 'auto') {
-            setDarkMode(preferredDark.value);
-          } else {
-            setDarkMode(darkMode.value);
-          }
-        });
-      });
-
-      app.runWithContext(() => {
-        tryOnScopeDispose(() => {
-          scope.stop();
-        });
-      });
-    }
-  };
-
-  return manatsu;
-}
--- packages/manatsu/src/plugin/index.ts
+++ packages/manatsu/src/plugin/index.ts
@@ -1,1 +1,29 @@
-export * from './dark-mode';
+import { usePreferredDark } from '@vueuse/core';
+import { type Plugin, ref, watchEffect } from 'vue';
+import { darkModeKey } from '../symbol';
+import { setDarkMode } from './dark-mode';
+
+export interface ManatsuOptions {
+  /** @default 'auto' */
+  darkMode?: boolean | 'auto';
+}
+
+export function createManatsu(options: ManatsuOptions = {}): Plugin {
+  const manatsu: Plugin = {
+    install(app) {
+      const darkMode = ref(options.darkMode ?? 'auto');
+      const preferredDark = usePreferredDark();
+      app.provide(darkModeKey, darkMode);
+
+      watchEffect(() => {
+        if (darkMode.value === 'auto') {
+          setDarkMode(preferredDark.value);
+        } else {
+          setDarkMode(darkMode.value);
+        }
+      });
+    }
+  };
+
+  return manatsu;
+}
