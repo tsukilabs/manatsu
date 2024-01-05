d0709952912eb3311752e7115979ab30cb1e3921
Andrew Ferreira <andrew.shien2@gmail.com>
2024-01-04 23:29:48 -0300

    chore: bump version

---
+++ taze.config.js
@@ -0,0 +1,11 @@
+import { defineConfig } from 'taze';
+
+export default defineConfig({
+  force: true,
+  write: true,
+  includeLocked: true,
+  install: false,
+  packageMode: {
+    vue: 'ignore'
+  }
+});
