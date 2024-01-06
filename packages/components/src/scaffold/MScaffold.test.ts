4ed8c2efe4d398c7dae4730663fb457e6c70378e
Andrew Ferreira <andrew.shien2@gmail.com>
2024-01-06 20:42:52 -0300

    chore: bump version

--- packages/components/src/scaffold/MScaffold.test.ts
+++ packages/components/src/scaffold/MScaffold.test.ts
@@ -26,4 +26,25 @@ describe('scaffold', () => {
 
     expect(wrapper.find('.m-scaffold-header').exists()).toBe(false);
   });
+
+  it('should have footer', () => {
+    const wrapper = mount(MScaffold, {
+      slots: {
+        footer: () => h('div', { id: 'footer-slot' })
+      }
+    });
+
+    expect(wrapper.find('.m-scaffold-footer').exists()).toBe(true);
+    expect(wrapper.find('#footer-slot').exists()).toBe(true);
+  });
+
+  it('should not have footer', () => {
+    const wrapper = mount(MScaffold, {
+      slots: {
+        default: () => h('div', 'Default slot')
+      }
+    });
+
+    expect(wrapper.find('.m-scaffold-footer').exists()).toBe(false);
+  });
 });
