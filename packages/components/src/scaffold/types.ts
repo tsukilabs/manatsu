d722947e36603ade02da73669ed1e103b6128221
Andrew Ferreira <andrew.shien2@gmail.com>
2024-01-06 22:58:27 -0300

    chore: bump version

--- packages/components/src/scaffold/types.ts
+++ packages/components/src/scaffold/types.ts
@@ -1,7 +1,10 @@
-import type { StyleValue } from 'vue';
+import type { StyleValue, VNode } from 'vue';
+import type { RouteLocationRaw } from 'vue-router';
 
 export interface SidebarItem {
   key: string;
+  label?: string | (() => VNode);
+  to?: RouteLocationRaw;
 }
 
 export interface ScaffoldProps {
