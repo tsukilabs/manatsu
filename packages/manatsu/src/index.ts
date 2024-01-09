7891927c2bab2024d89e2f1416734919391e9864
Andrew Ferreira <andrew.shien2@gmail.com>
2024-01-09 10:34:31 -0300

    feat(core): expose dark mode helpers

--- packages/manatsu/src/index.ts
+++ packages/manatsu/src/index.ts
@@ -1,7 +1,8 @@
 import './style/main.scss';
 
-export * from './plugin';
+export { createManatsu } from './plugin';
 export * from '@manatsu/components/src/index.ts';
 export * from '@manatsu/composables/src/index.ts';
 
 export type { MaybeNullishRef } from '@manatsu/shared';
+export { isDarkMode, toggleDarkMode } from '@manatsu/shared';
