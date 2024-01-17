af983a33447136d8c93a7c9f1f5f7eb1dba50e13
Andrew Ferreira <andrew.shien2@gmail.com>
2024-01-17 10:32:03 -0300

    feat(playground): save window state

--- packages/playground/src-tauri/src/main.rs
+++ packages/playground/src-tauri/src/main.rs
@@ -2,11 +2,16 @@
 
 #[tauri::command]
 fn footer_text() -> String {
-  String::from("Copyright © 2024 Manatsu")
+  use chrono::Datelike;
+
+  let current_date = chrono::Local::now();
+  let year = current_date.year();
+  format!("Copyright © {} Manatsu", year)
 }
 
 fn main() {
   tauri::Builder::default()
+    .plugin(tauri_plugin_window_state::Builder::default().build())
     .plugin(tauri_plugin_manatsu::init())
     .invoke_handler(tauri::generate_handler![footer_text])
     .run(tauri::generate_context!())
