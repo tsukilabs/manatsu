47f22a6e26fdaddb95f5c1ac973928ccc7985606
Andrew Ferreira <andrew.shien2@gmail.com>
2024-01-17 12:31:19 -0300

    fix(components): change elevated card bg color

--- packages/components/src/card/MCard.vue
+++ packages/components/src/card/MCard.vue
@@ -113,9 +113,9 @@ const mediaOrder = computed(() => {
 
 .m-card-elevated {
   box-shadow:
-    0 1px 2px 0 rgba(0 0 0 / 30%),
-    0 1px 3px 1px rgba(0 0 0 / 15%);
-  background-color: var(--m-color-surface-container-low);
+    0 1px 2px 0 rgb(0 0 0 / 30%),
+    0 1px 3px 1px rgb(0 0 0 / 15%);
+  background-color: var(--m-color-surface-container-high);
 }
 
 .m-card-outlined {
