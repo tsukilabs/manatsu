a1272a382a47526e83dd8f366556cce1ca857c15
Andrew Ferreira <andrew.shien2@gmail.com>
2024-01-18 11:22:26 -0300

    chore: bump deps

--- taze.config.js
+++ taze.config.js
@@ -4,9 +4,5 @@ export default defineConfig({
   force: true,
   write: true,
   includeLocked: true,
-  recursive: true,
-  packageMode: {
-    vue: 'ignore',
-    '@vitejs/plugin-vue': 'ignore'
-  }
+  recursive: true
 });
