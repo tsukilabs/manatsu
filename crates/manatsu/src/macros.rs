9c622289fcfc40c26ee2aa199a485d69dc11ea5a
Andrew Ferreira <andrew.shien2@gmail.com>
2024-01-22 18:01:41 -0300

    feat(cli): update `tauri.conf.json` after project creation

--- crates/manatsu/src/macros.rs
+++ crates/manatsu/src/macros.rs
@@ -1,3 +1,38 @@
+/// Executes `cargo` with the specified arguments.
+///
+/// # Examples
+///
+/// ```
+/// let output = manatsu::cargo!("--version").unwrap();
+/// assert!(output.status.success());
+/// ```
+#[macro_export]
+macro_rules! cargo {
+  ($( $arg:literal ),*) => {{
+    let mut args: Vec<&str> = Vec::new();
+    $( args.push($arg); )*
+
+    $crate::output!(std::process::Command::new("cargo"), args)
+  }};
+  ($args:expr) => {{
+    $crate::output!(std::process::Command::new("cargo"), $args)
+  }};
+}
+
+/// Executes `pnpm` with the specified arguments.
+#[macro_export]
+macro_rules! pnpm {
+  ($( $arg:literal ),*) => {{
+    let mut args: Vec<&str> = Vec::new();
+    $( args.push($arg); )*
+
+    $crate::output!($crate::win_cmd!("pnpm"), args)
+  }};
+  ($args:expr) => {{
+    $crate::output!($crate::win_cmd!("pnpm"), $args)
+  }};
+}
+
 /// Wrap [std::process::Command], executing `cmd` as the program if the current OS is Windows.
 ///
 /// This is only useful in some very specific cases.
@@ -19,50 +54,11 @@ macro_rules! win_cmd {
   }};
 }
 
-/// Executes `cargo` with the specified arguments.
-///
-/// # Examples
-///
-/// ```
-/// let output = manatsu::cargo!("--version").unwrap();
-/// assert!(output.status.success());
-/// ```
+#[doc(hidden)]
 #[macro_export]
-macro_rules! cargo {
-  ($( $arg:literal ),*) => {{
-    let mut args: Vec<&str> = Vec::new();
-    $( args.push($arg); )*
-
-    std::process::Command::new("cargo")
-      .args(args)
-      .stderr(std::process::Stdio::inherit())
-      .stdout(std::process::Stdio::inherit())
-      .output()
-  }};
-  ($args:expr) => {{
-    std::process::Command::new("cargo")
-      .args($args)
-      .stderr(std::process::Stdio::inherit())
-      .stdout(std::process::Stdio::inherit())
-      .output()
-  }};
-}
-
-/// Executes `pnpm` with the specified arguments.
-#[macro_export]
-macro_rules! pnpm {
-  ($( $arg:literal ),*) => {{
-    let mut args: Vec<&str> = Vec::new();
-    $( args.push($arg); )*
-
-    $crate::win_cmd!("pnpm")
-      .args(args)
-      .stderr(std::process::Stdio::inherit())
-      .stdout(std::process::Stdio::inherit())
-      .output()
-  }};
-  ($args:expr) => {{
-    $crate::win_cmd!("pnpm")
+macro_rules! output {
+  ($cmd:expr, $args:expr) => {{
+    $cmd
       .args($args)
       .stderr(std::process::Stdio::inherit())
       .stdout(std::process::Stdio::inherit())
--- crates/manatsu/src/project/manifest.rs
+++ crates/manatsu/src/project/manifest.rs
@@ -1,127 +1,111 @@
 use super::Project;
 use anyhow::Result;
+use convert_case::{Case, Casing};
 use globset::Glob;
-use serde::{Deserialize, Serialize};
 use std::fs;
 use std::path::Path;
 use walkdir::WalkDir;
 
-pub trait Manifest {
-  fn update_metadata<P: AsRef<Path>>(dir_path: P, project: &Project) -> Result<()>;
-}
+const BASE_VERSION: &str = "0.1.0";
 
-#[derive(Deserialize, Serialize)]
-#[serde(rename_all(serialize = "snake_case", deserialize = "camelCase"))]
-pub(super) struct PackageJson {
-  name: String,
-  description: String,
-  version: String,
-}
-
-impl PackageJson {
-  fn read<P: AsRef<Path>>(path: P) -> Result<serde_json::Value> {
-    let json = fs::read_to_string(path)?;
-    let package: serde_json::Value = serde_json::from_str(&json)?;
-    Ok(package)
-  }
-}
-
-impl Manifest for PackageJson {
-  fn update_metadata<P: AsRef<Path>>(dir_path: P, project: &Project) -> Result<()> {
-    let path = dir_path.as_ref().join("package.json");
-    let mut package = Self::read(&path)?;
-
-    macro_rules! update {
-      ($key:literal, $value:expr) => {
-        package[$key] = serde_json::Value::String($value);
-      };
-    }
-
-    update!("name", project.name.clone());
-    update!("version", "0.1.0".to_string());
-
-    if let Some(description) = &project.description {
-      update!("description", description.clone());
+macro_rules! clone_or_empty {
+  ($value:expr) => {
+    if let Some(value) = $value {
+      value.clone()
     } else {
-      update!("description", String::from(""));
+      String::from("")
     }
+  };
+}
 
-    let json = serde_json::to_string_pretty(&package)?;
-    fs::write(path, json)?;
+pub fn update_package_json<P: AsRef<Path>>(dir_path: P, project: &Project) -> Result<()> {
+  let path = dir_path.as_ref().join("package.json");
+  let package = fs::read_to_string(&path)?;
+  let mut package: serde_json::Value = serde_json::from_str(&package)?;
 
-    Ok(())
+  macro_rules! update {
+    ($key:literal, $value:expr) => {
+      package[$key] = serde_json::Value::String($value);
+    };
   }
+
+  update!("name", project.name.clone());
+  update!("version", BASE_VERSION.to_string());
+  update!("description", clone_or_empty!(&project.description));
+
+  let json = serde_json::to_string_pretty(&package)?;
+  fs::write(path, json)?;
+
+  Ok(())
 }
 
-#[derive(Deserialize, Serialize)]
-pub(super) struct CargoToml {
-  package: Option<CargoPackage>,
-  workspace: Option<CargoWorkspace>,
-}
+pub fn update_cargo_toml<P: AsRef<Path>>(dir_path: P, project: &Project) -> Result<()> {
+  let glob = Glob::new("**/Cargo.toml")?.compile_matcher();
+  for entry in WalkDir::new(dir_path).into_iter().filter_map(|e| e.ok()) {
+    let path = entry.path();
 
-#[derive(Deserialize, Serialize)]
-struct CargoWorkspace {
-  package: Option<CargoPackage>,
-}
+    if glob.is_match(path) {
+      let cargo_toml = fs::read_to_string(path)?;
+      let mut cargo_toml: toml::Value = toml::from_str(&cargo_toml)?;
 
-#[derive(Deserialize, Serialize)]
-struct CargoPackage {
-  name: Option<String>,
-  description: Option<String>,
-  version: Option<String>,
-}
-
-impl Manifest for CargoToml {
-  fn update_metadata<P: AsRef<Path>>(dir_path: P, project: &Project) -> Result<()> {
-    let glob = Glob::new("**/Cargo.toml")?.compile_matcher();
-    for entry in WalkDir::new(dir_path).into_iter().filter_map(|e| e.ok()) {
-      let path = entry.path();
-
-      if glob.is_match(path) {
-        let cargo_toml = fs::read_to_string(path)?;
-        let mut cargo_toml: toml::Value = toml::from_str(&cargo_toml)?;
-
-        macro_rules! update {
-          ($key:literal, $value:expr) => {
-            if cargo_toml["package"].get($key).is_some() {
-              cargo_toml["package"][$key] = toml::Value::String($value);
-            }
-          };
-        }
-
-        macro_rules! update_workspace {
-          ($key:literal, $value:expr) => {
-            if cargo_toml["workspace"]["package"].get($key).is_some() {
-              cargo_toml["workspace"]["package"][$key] = toml::Value::String($value);
-            }
-          };
-        }
-
-        let description = if let Some(desc) = &project.description {
-          desc.clone()
-        } else {
-          String::from("")
-        };
-
-        if cargo_toml.get("package").is_some() {
-          update!("name", project.name.clone());
-          update!("version", "0.1.0".to_string());
-          update!("description", description.clone());
-        }
-
-        if let Some(workspace) = cargo_toml.get("workspace") {
-          if workspace.get("package").is_some() {
-            update_workspace!("name", project.name.clone());
-            update_workspace!("version", "0.1.0".to_string());
-            update_workspace!("description", description);
+      macro_rules! update {
+        ($key:literal, $value:expr) => {
+          if cargo_toml["package"].get($key).is_some() {
+            cargo_toml["package"][$key] = toml::Value::String($value);
           }
-        }
-
-        let cargo_toml = toml::to_string_pretty(&cargo_toml)?;
-        fs::write(path, cargo_toml)?;
+        };
       }
-    }
 
-    Ok(())
+      macro_rules! update_workspace {
+        ($key:literal, $value:expr) => {
+          if cargo_toml["workspace"]["package"].get($key).is_some() {
+            cargo_toml["workspace"]["package"][$key] = toml::Value::String($value);
+          }
+        };
+      }
+
+      if cargo_toml.get("package").is_some() {
+        update!("name", project.name.clone());
+        update!("version", BASE_VERSION.to_string());
+        update!("description", clone_or_empty!(&project.description));
+      }
+
+      if let Some(workspace) = cargo_toml.get("workspace") {
+        if workspace.get("package").is_some() {
+          update_workspace!("name", project.name.clone());
+          update_workspace!("version", BASE_VERSION.to_string());
+          update_workspace!("description", clone_or_empty!(&project.description));
+        }
+      }
+
+      let cargo_toml = toml::to_string_pretty(&cargo_toml)?;
+      fs::write(path, cargo_toml)?;
+    }
   }
+
+  Ok(())
+}
+
+pub fn update_tauri_conf<P: AsRef<Path>>(dir_path: P, project: &Project) -> Result<()> {
+  let path = dir_path.as_ref().join("src-tauri").join("tauri.conf.json");
+  let tauri_conf = fs::read_to_string(&path)?;
+  let mut tauri_conf: serde_json::Value = serde_json::from_str(&tauri_conf)?;
+
+  macro_rules! update {
+    ($key:literal, $value:expr) => {
+      tauri_conf["package"][$key] = serde_json::Value::String($value);
+    };
+  }
+
+  update!("productName", project.name.clone());
+  update!("version", BASE_VERSION.to_string());
+
+  let title = project.name.to_case(Case::Title);
+  let title = serde_json::Value::String(title);
+  tauri_conf["tauri"]["windows"][0]["title"] = title;
+
+  let tauri_conf = serde_json::to_string_pretty(&tauri_conf)?;
+  fs::write(path, tauri_conf)?;
+
+  Ok(())
 }
