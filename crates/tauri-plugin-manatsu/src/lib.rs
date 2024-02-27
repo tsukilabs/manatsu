cff0053fc1c2d26a7054d877e539b5e12dd891aa
Andrew Ferreira <andrew.shien2@gmail.com>
2024-02-27 11:16:39 -0300

    chore: bump dependencies

--- crates/tauri-plugin-manatsu/src/lib.rs
+++ crates/tauri-plugin-manatsu/src/lib.rs
@@ -1,15 +1,14 @@
 mod command;
 
-use command::*;
 use tauri::plugin::{Builder, TauriPlugin};
 use tauri::Runtime;
 
 pub fn init<R: Runtime>() -> TauriPlugin<R> {
   Builder::new("manatsu")
     .invoke_handler(tauri::generate_handler![
-      random_hex_color,
-      random_hsl_color,
-      random_rgb_color
+      command::random_hex_color,
+      command::random_hsl_color,
+      command::random_rgb_color
     ])
     .build()
 }
