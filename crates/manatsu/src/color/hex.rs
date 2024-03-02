84ff454a31f90139586524d00cfec3ec260d9381
Andrew Ferreira <andrew.shien2@gmail.com>
2024-03-02 16:25:08 -0300

    refactor(cli): remove unnecessary `to_string` call

--- crates/manatsu/src/color/hex.rs
+++ crates/manatsu/src/color/hex.rs
@@ -92,7 +92,7 @@ impl From<Hex> for String {
 
 impl fmt::Display for Hex {
   fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-    write!(f, "{}", self.clone().to_string())
+    write!(f, "{}", self.clone())
   }
 }
 
