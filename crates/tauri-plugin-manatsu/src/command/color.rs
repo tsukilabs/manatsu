ca560bfbfc81d08750dfb4bbc18c0a85db987e28
Andrew Ferreira <andrew.shien2@gmail.com>
2024-03-04 11:34:24 -0300

    chore: bump version

--- crates/tauri-plugin-manatsu/src/command/color.rs
+++ crates/tauri-plugin-manatsu/src/command/color.rs
@@ -3,69 +3,69 @@ use crate::Result;
 use manatsu::color::{Color, Hex, Hsl, Rgb};
 
 #[tauri::command]
-pub fn string_to_hex(color: String) -> Result<Hex> {
+pub async fn string_to_hex(color: String) -> Result<Hex> {
   Hex::try_from(color.as_str()).map_err(|e| Error::Color(e.to_string()))
 }
 
 #[tauri::command]
-pub fn hex_to_rgb(hex: Hex) -> Rgb {
+pub async fn hex_to_rgb(hex: Hex) -> Rgb {
   hex.into()
 }
 
 #[tauri::command]
-pub fn hex_to_hsl(hex: Hex) -> Hsl {
+pub async fn hex_to_hsl(hex: Hex) -> Hsl {
   hex.into()
 }
 
 #[tauri::command]
-pub fn hex_to_string(hex: Hex) -> String {
+pub async fn hex_to_string(hex: Hex) -> String {
   hex.into()
 }
 
 #[tauri::command]
-pub fn rgb_to_hex(rgb: Rgb) -> Hex {
+pub async fn rgb_to_hex(rgb: Rgb) -> Hex {
   rgb.into()
 }
 
 #[tauri::command]
-pub fn rgb_to_hsl(rgb: Rgb) -> Hsl {
+pub async fn rgb_to_hsl(rgb: Rgb) -> Hsl {
   rgb.into()
 }
 
 #[tauri::command]
-pub fn rgb_to_string(rgb: Rgb) -> String {
+pub async fn rgb_to_string(rgb: Rgb) -> String {
   rgb.into()
 }
 
 #[tauri::command]
-pub fn hsl_to_hex(hsl: Hsl) -> Hex {
+pub async fn hsl_to_hex(hsl: Hsl) -> Hex {
   hsl.into()
 }
 
 #[tauri::command]
-pub fn hsl_to_rgb(hsl: Hsl) -> Rgb {
+pub async fn hsl_to_rgb(hsl: Hsl) -> Rgb {
   hsl.into()
 }
 
 #[tauri::command]
-pub fn hsl_to_string(hsl: Hsl) -> String {
+pub async fn hsl_to_string(hsl: Hsl) -> String {
   hsl.into()
 }
 
 /// Generate a random hex color.
 #[tauri::command]
-pub fn random_hex_color() -> String {
+pub async fn random_hex_color() -> String {
   Hex::random().into()
 }
 
 /// Generate a random HSL color.
 #[tauri::command]
-pub fn random_hsl_color() -> String {
+pub async fn random_hsl_color() -> String {
   Hsl::random().into()
 }
 
 /// Generate a random RGB color.
 #[tauri::command]
-pub fn random_rgb_color() -> String {
+pub async fn random_rgb_color() -> String {
   Rgb::random().into()
 }
