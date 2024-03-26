0c0205f4d3aab016d5ee7fecdb3fd0b56b9aa32a
Andrew Ferreira <andrew.shien2@gmail.com>
2024-03-26 18:46:22 -0300

    test(composables): fix `useInvoke` tests

--- packages/composables/src/useInvoke/index.test.ts
+++ packages/composables/src/useInvoke/index.test.ts
@@ -31,53 +31,56 @@ describe('useInvoke', () => {
 
   it('should invoke a command', async () => {
     const loading = ref(false);
-    const result = useInvoke(Command.RandomStringHexColor, '#FFFFFF', { loading });
-    expect(result.value).toBe('#FFFFFF');
+    const { state } = useInvoke(Command.RandomStringHexColor, '#FFFFFF', { loading });
+    expect(state.value).toBe('#FFFFFF');
 
     await nextTick();
     await until(loading).not.toBeTruthy({ timeout: 50, throwOnTimeout: true });
 
-    expect(result.value).toBe('#000000');
+    expect(state.value).toBe('#000000');
   });
 
   it('should not execute immediately', async () => {
     const loading = ref(false);
-    const result = useInvoke(Command.RandomStringHexColor, null, { loading, lazy: true });
-    expect(result.value).toBeNull();
+    const { state } = useInvoke(Command.RandomStringHexColor, null, { loading, lazy: true });
+    expect(state.value).toBeNull();
 
     await nextTick();
     await until(loading).not.toBeTruthy({ timeout: 50, throwOnTimeout: true });
 
-    expect(result.value).toBeNull();
+    expect(state.value).toBeNull();
   });
 
   it('should execute manually', async () => {
     const loading = ref(false);
-    const result = useInvoke(Command.RandomStringHexColor, null, { loading, lazy: true });
-    expect(result.value).toBeNull();
+    const { state, execute } = useInvoke(Command.RandomStringHexColor, null, {
+      loading,
+      lazy: true
+    });
+    expect(state.value).toBeNull();
 
-    await result.execute();
+    await execute();
     await nextTick();
     await until(loading).not.toBeTruthy({ timeout: 50, throwOnTimeout: true });
 
-    expect(result.value).toBe('#000000');
+    expect(state.value).toBe('#000000');
   });
 
   it('should invoke again if the command changes', async () => {
     const loading = ref(false);
     const command = ref<Command>(Command.RandomStringHexColor);
-    const result = useInvoke(command, '#FFFFFF', { loading });
+    const { state } = useInvoke(command, '#FFFFFF', { loading });
 
     await nextTick();
     await until(loading).not.toBeTruthy({ timeout: 50, throwOnTimeout: true });
 
-    expect(result.value).toBe('#000000');
+    expect(state.value).toBe('#000000');
 
     command.value = Command.RandomStringRgbColor;
     await nextTick();
     await until(loading).not.toBeTruthy({ timeout: 50, throwOnTimeout: true });
 
-    expect(result.value).toBe('rgb(0, 0, 0)');
+    expect(state.value).toBe('rgb(0, 0, 0)');
   });
 
   it('should throw if the command is invalid', async () => {
