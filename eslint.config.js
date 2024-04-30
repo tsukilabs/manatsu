99b0663bea3971ce5dee0e557a110d2d55887b0c
Andrew Ferreira <andrew.shien2@gmail.com>
2024-04-30 09:02:50 -0300

    chore: bump dependencies

--- eslint.config.js
+++ eslint.config.js
@@ -2,7 +2,6 @@ import config from '@tb-dev/eslint-config';
 
 export default config({
   vitest: true,
-  vue: true,
   project: [
     'tsconfig.json',
     'packages/composables/tsconfig.json',
@@ -18,12 +17,6 @@ export default config({
     typescript: {
       '@typescript-eslint/no-explicit-any': 'off',
       '@typescript-eslint/no-unsafe-member-access': 'off'
-    },
-    vue: {
-      'vue/block-lang': [
-        'error',
-        { script: { lang: 'ts' }, style: { lang: 'scss', allowNoLang: false } }
-      ]
     }
   }
 });
