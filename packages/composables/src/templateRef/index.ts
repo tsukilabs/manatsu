93dac6368dc506d84f4f38f9088e97271b854440
Andrew Ferreira <andrew.shien2@gmail.com>
2024-06-21 14:13:18 -0300

    chore: bump version

--- packages/composables/src/templateRef/index.ts
+++ packages/composables/src/templateRef/index.ts
@@ -1,5 +1,9 @@
-import { type Ref, ref } from 'vue';
+import { type ComponentPublicInstance, type Ref, ref } from 'vue';
 
 export function templateRef<T extends HTMLElement>(): Ref<T | null> {
+  return ref(null);
+}
+
+export function componentRef<T extends ComponentPublicInstance>(): Ref<T | null> {
   return ref(null);
 }
