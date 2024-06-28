5581eb4628e3810588c191c8f215bfdfc353b63d
Andrew Ferreira <andrew.shien2@gmail.com>
2024-06-28 16:27:58 -0300

    feat: add `AppHandleExt` trait

--- crates/tauri-plugin-manatsu/src/lib.rs
+++ crates/tauri-plugin-manatsu/src/lib.rs
@@ -1,16 +1,27 @@
 mod command;
 mod error;
-mod log;
+pub mod log;
 
 pub use error::Error;
-pub use log::{date, Log, VersionSnapshot};
+pub use log::{Log, VersionSnapshot};
 
+use error::Result;
 use log::LogCache;
 use std::sync::Mutex;
 use tauri::plugin::{Builder, TauriPlugin};
-use tauri::{Manager, RunEvent, Runtime};
+use tauri::{AppHandle, Manager, RunEvent, Runtime};
 
 pub const VERSION: &str = env!("CARGO_PKG_VERSION");
+
+pub trait AppHandleExt {
+  fn write_logs_to_disk(&self) -> Result<()>;
+}
+
+impl<R: Runtime> AppHandleExt for AppHandle<R> {
+  fn write_logs_to_disk(&self) -> Result<()> {
+    Log::write_to_disk(self)
+  }
+}
 
 pub fn init<R: Runtime>() -> TauriPlugin<R> {
   Builder::new("manatsu")
